3ae19a882c0cb5a6971851254dd96911c0c04f0e
Prizm Moon <prdigitalmoon@gmail.com>
2021-12-08 15:10:30 -0500

    Adds description list with field names

--- public/salamanders/show.php
+++ public/salamanders/show.php
@@ -18,6 +18,25 @@ include(SHARED_PATH . '/salamanderHeader.php');
 
 <a href="<?= url_for('/salamanders/index.php'); ?>">&laquo; Back to List</a>
 
+<h1>Salamander: <?= h($salamander['name']) ?></h1>
+
+<dl>
+  <dt>ID</dt>
+  <dd><?= h($salamander['id']) ?></dd>
+</dl>
+<dl>
+  <dt>Name</dt>
+  <dd><?= h($salamander['name']) ?></dd>
+</dl>
+<dl>
+  <dt>Habitat</dt>
+  <dd><?= h($salamander['habitat']) ?></dd>
+</dl>
+<dl>
+  <dt>Description</dt>
+  <dd><?= h($salamander['description']) ?></dd>
+</dl>
+
 Page ID: <?= h($id); ?>
 
 <?php include(SHARED_PATH . '/salamanderFooter.php'); ?>
